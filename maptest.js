8eb6c92d0aa96b29af484a6c741ce940c533117c
Fredrick Khoury <fredk1018@gmail.com>
2015-04-20 15:25:51 -0500

    current

--- maptest.js
+++ maptest.js
@@ -1,17 +1,23 @@
 if (Meteor.isClient) {
   Template.maps.onRendered(function(){
-     initMap();
-    })
-initMap = function(){
-    var map;
-    function initialize() {
-      var mapOptions = {
-        zoom: 8,
-        center: new google.maps.LatLng(-34.397, 150.644)
-      };
-      map = new google.maps.Map(document.getElementById('map-canvas'),
-  mapOptions);
-}
 
-}
+initMap();
+
+})
+ 
+ 
+initMap = function(){
+
+var map;
+var mapOptions = {
+zoom: 8,
+center: new google.maps.LatLng(-34.397, 150.644)
+};
+map = new google.maps.Map(document.getElementById('map-canvas'),
+mapOptions);
+
+    }
+
+
+
 }
