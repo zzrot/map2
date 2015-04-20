f4e4edd4bc3a2dc6121ae8be58e0a73adf88aeba
Fredrick Khoury <fredk1018@gmail.com>
2015-04-20 13:53:34 -0500

    ethaan2

--- maptest.js
+++ maptest.js
@@ -1,26 +1,19 @@
 if (Meteor.isClient) {
-  
-  Template.maps.onRendered = function() {
+  Template.maps.onRendered(function(){
+     initMap();
+    })
+initMap = function(){
     var map;
-function initialize() {
-  console.log('INSIDE INITIALIZE');
-  var mapOptions = {
-    zoom: 8,
-    center: new google.maps.LatLng(-34.397, 150.644)
-  };
-  map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
+    function initialize() {
+      var mapOptions = {
+        zoom: 8,
+        center: new google.maps.LatLng(-34.397, 150.644)
+      };
+      map = new google.maps.Map(document.getElementById('map-canvas'),
+  mapOptions);
 }
 
-google.maps.event.addDomListener(window, 'load', initialize);
-      
 }
-
-
-
-
-}
-
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
